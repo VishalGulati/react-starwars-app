15369e63ee4719c727e06d2958b83d4beff85759
Vishal Gulati <vishal.rvg@gmail.com>
2019-11-17 21:46:54 +0530

    Updates

---
+++ precache-manifest.ce8f62ef048b1e07f234df4ce1ed258b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1c7185ac8da45608363d8c3fad0104",
+    "url": "/react-starwars-app/index.html"
+  },
+  {
+    "revision": "ddb39390dee39131d9d5",
+    "url": "/react-starwars-app/static/css/main.d37fe46b.chunk.css"
+  },
+  {
+    "revision": "97e422116401a5c497ff",
+    "url": "/react-starwars-app/static/js/2.33f92e5e.chunk.js"
+  },
+  {
+    "revision": "ddb39390dee39131d9d5",
+    "url": "/react-starwars-app/static/js/main.a4a50615.chunk.js"
+  },
+  {
+    "revision": "000ddcb0ca0ea1e0803e",
+    "url": "/react-starwars-app/static/js/runtime-main.f961a6f1.js"
+  }
+]);
